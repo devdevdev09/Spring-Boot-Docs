accccce267afa149681e03835c2eef0fec58246c
dev09 <devdevdevdevdev09@gmail.com>
2020-10-12 17:50:07 +0900

    init

---
+++ src/main/java/com/spring/boot/rest/docs/springbootrestdocs/SpringBootRestDocsApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.rest.docs.springbootrestdocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestDocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestDocsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/boot/rest/docs/springbootrestdocs/SpringBootRestDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.rest.docs.springbootrestdocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestDocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
